21cc64e931cf4b203744269a048c7cc0122ca67f
Daniel vieira <danielvieira.2007@hotmail.com>
2021-10-21 00:27:31 -0300

    first commit

---
+++ primeiroProjeto/primeiroProjeto/Program.cs
@@ -0,0 +1,278 @@
+﻿using System;
+
+namespace primeiroProjeto
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Exercicio valor hora
+            //Console.WriteLine("Insira o seu nome:");
+            //var nome = Console.ReadLine();
+            //Console.WriteLine("Isira o total de horas trabalhada:");
+            //int total_Horas =  int.Parse(Console.ReadLine());
+            //Console.WriteLine("Insira o valor da hora:");
+            //int valor_Hora = int.Parse(Console.ReadLine());
+
+            //total_Horas = total_Horas * valor_Hora;
+
+            //Console.WriteLine($"O salário de {nome} é R${total_Horas}");
+
+            //Exercicio de variavel auxiliar
+            //int x = 5;
+            //int y = 7;
+
+            //int aux;
+            //aux = x;
+            //x = y;
+            //y = aux;
+
+            //Console.WriteLine(x);
+            //Console.WriteLine(y);
+
+            //salario + porcentagem
+            //double salario, percentual;
+            //Console.WriteLine("Informe o salário atual: ");
+            //salario = double.Parse(Console.ReadLine());
+
+
+
+            //Console.WriteLine("Informe o percentual de aumento: ");
+            //percentual = double.Parse(Console.ReadLine());
+
+
+
+            //salario = salario + (salario * percentual) / 100;
+
+
+
+            //Console.WriteLine("Novo salário: " + salario);
+
+            //Suponha que um vendedor de carros trabalhe em uma empresa onde todos os veículos custem 
+            //30.000 reais.Faça um programa que leia o nome do funcionário, o seu salario base(Exemplo: 1000),
+            //e a quantidade de veículos vendidos. Para cada veiculo vendido ele recebe 15 % de comissão, que deve 
+            //    ser somado ao seu salario.Apresente o novo salario da mesma forma do exercício anterior.
+
+            //Console.WriteLine("Digite seu nome:");
+            //string nome = Console.ReadLine();
+            //Console.WriteLine("Digite seu salario");
+            //double salario = double.Parse(Console.ReadLine());
+            //Console.WriteLine("Digite a quantidade de veiculos vendidos");
+            //double qtdeVeiculos = double.Parse(Console.ReadLine());
+
+            //double valorVeiculo = 30000, percentualVeiculo = 15;
+
+
+            //double totalSalario = salario + ( qtdeVeiculos  * (valorVeiculo * percentualVeiculo / 100));
+
+            //Console.WriteLine($"{nome} seu novo salario é {totalSalario}");
+
+            //exercicio:
+            //Para uma eleição, receba o numero de eleitores, a quantidade de votos brancos,
+            //nulos e validos. Mostre o percentual de cada tipo de voto.
+
+            //Console.WriteLine("Digite a quantidade de votos em brancos");
+            //int votosBrancos = int.Parse(Console.ReadLine());
+            //Console.WriteLine("Digite a quantidade de votos nulos");
+            //int votosNulos = int.Parse(Console.ReadLine());
+            //Console.WriteLine("Digite a quantidade de votos validos");
+            //int votosValidos = int.Parse(Console.ReadLine());
+
+
+            //int totalVotos = votosValidos + votosNulos + votosBrancos;
+
+            //votosBrancos = (votosBrancos * 100) / totalVotos;
+            //votosNulos = (votosNulos * 100) / totalVotos;
+            //votosValidos = (votosValidos * 100) / totalVotos;
+
+
+            //Console.WriteLine($"Votos Brancos: {votosBrancos}%, Votos Nulos: {votosNulos}%, Votos Validos: {votosValidos}% Total de Votos: {totalVotos}");
+
+            //exercicio
+            //Ler um numero e mostrar seu antecessor e seu sucessor.
+
+            //Console.WriteLine("Digite um numero:");
+            //int numero = int.Parse(Console.ReadLine());
+
+            //int antecessor = numero - 1;
+            //int sucessor = numero + 1;
+
+            //Console.WriteLine($"O seu numero e {numero} seu antecessor e {antecessor} seu sucessor e {sucessor}");
+
+            //Exercicio media das notas    
+
+            //Console.WriteLine("Digite a primeira nota");
+            //double nota1 = double.Parse(Console.ReadLine());
+            //Console.WriteLine("Digite a segunda nota");
+            //double nota2 = double.Parse(Console.ReadLine());
+            //Console.WriteLine("Digite a terceira nota");
+            //double nota3 = double.Parse(Console.ReadLine());
+
+            //double media = (nota1 + nota2 + nota3) / 3;
+
+            //Console.WriteLine($"A media das notas e: {media}");
+
+            //Console.WriteLine("Digite um valor:");
+            //int valor = int.Parse(Console.ReadLine());
+
+            //if (valor > 0)
+            //{
+            //    Console.WriteLine("O valor e positivo!");
+            //}else if (valor < 0)
+            //{
+            //    Console.WriteLine("O valor e negatico");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("O valor e zero");  
+            //}
+
+            //int x = 5;
+            //Console.WriteLine("Digite um valor:");
+            //int valor = int.Parse(Console.ReadLine());
+
+
+            //if(valor == x)
+            //{
+            //    Console.WriteLine("O valor e igual");
+
+            //}
+            //else
+            //{
+            //    Console.WriteLine("O valor e diferente");
+            //}
+
+
+            //int x = 5;
+            //Console.WriteLine("Digite um valor:");
+            //int valor = int.Parse(Console.ReadLine());
+
+
+            //if (valor == x)
+            //{
+            //    Console.WriteLine("O valor e igual");
+
+            //}
+            //else
+            //{
+            //    if(valor < x)
+            //    {
+            //        Console.WriteLine("O seu numero e menor que o guardado");
+            //    }else
+            //    {
+            //        Console.WriteLine("O seu numero e maior que o numero guardado ");
+            //    }
+            //}
+
+            //Leia 2 números do usuário, após isso, mostre esta mensagem ao usuário:
+            //“Informe + para soma, - para subtração, * para multiplicação e / para divisão”
+            //Leia a escolha do usuário e faça o que ele pediu!
+
+            //Console.WriteLine("Digite o primeiro numero:");
+            //double n1 = double.Parse(Console.ReadLine());
+            //Console.WriteLine("Digite o segundo numero:");
+            //double n2 = double.Parse(Console.ReadLine());
+            //Console.WriteLine("Escolha a operacao + ou - ou * ou /");
+            //char operacao = char.Parse(Console.ReadLine());
+
+            //if(operacao == '+')
+            //{
+            //    double soma = n1 + n2;
+            //    Console.WriteLine($"A soma dos seus numeros e: {soma}");
+            //}else if(operacao == '-')
+            //{
+            //    double subtracao = n1 - n2;
+            //    Console.WriteLine($"A subtracao dos seus numeros e: {subtracao}");
+            //}else if(operacao == '*')
+            //{
+            //    double multiplicacao = n1 * n2;
+            //    Console.WriteLine($"A multiplicacao dos seus numeros e: {multiplicacao}");
+            //}else if(operacao == '/')
+            //{
+            //    double divisao = n1 / n2;
+            //    Console.WriteLine($"A divisao dos seus numeros e: {divisao}");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("Operacao invalida");
+            //}
+
+
+            //exercicio aumento de salario 
+
+            //Console.WriteLine("Insira o seu salario");
+            //double salario = double.Parse(Console.ReadLine());
+
+            //if (salario > 0 && salario <= 900)
+            //{
+            //    double novoSalario = salario + ((salario / 100) * 5);
+            //    Console.WriteLine($"O seu novo salario e: {novoSalario}");
+            //}
+            //else if (salario > 900 && salario <= 1400)
+            //{
+            //    double novoSalario = salario + ((salario / 100) * 8);
+            //    Console.WriteLine($"O seu novo salario e: {novoSalario}");
+            //}
+            //else if (salario > 1400)
+            //{
+            //    double novoSalario = salario + ((salario / 100) * 10);
+            //    Console.WriteLine($"O seu novo salario e: {novoSalario}");
+            //}
+
+            //Par ou impar
+
+            //int valor;
+
+
+            //Console.WriteLine("Informe um valor:");
+            //valor = int.Parse(Console.ReadLine());
+
+
+            //if (valor % 2 == 0)
+            //{
+            //    Console.WriteLine("O valor é par!");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("O valor é impar!");
+            //}
+
+             //exercicio classifique um triangulo
+
+            //Console.WriteLine("Insira o primeiro numero:");
+            //int n1 = int.Parse(Console.ReadLine());
+
+            //Console.WriteLine("Insira o segundo numero:");
+            //int n2 = int.Parse(Console.ReadLine());
+
+            //Console.WriteLine("Insira o terceiro numero:");
+            //int n3 = int.Parse(Console.ReadLine());
+            
+            //if(n1 < n2 + n3 && n2 < n3 + n1 && n3 < n2 + n1)
+            //{
+            //    if (n1 == n2 && n2 == n3 && n3 == n1)
+            //    {
+            //        Console.WriteLine("Triangulo Equilatero");
+
+            //    }else if (n1 == n2 || n2 == n3)
+            //    {
+            //        Console.WriteLine("Triangulo isosceles");
+
+            //    }else if (n1 != n2 && n2 != n3 && n3 != n1)
+            //    {
+            //        Console.WriteLine("Triangulo escaleno");
+            //    }
+            //}else
+            //{
+            //    Console.WriteLine("Nao classifica um triangulo");
+            //}
+
+
+                //Para doar sangue é necessário ter entre 18 e 67 anos.
+                //Faça um aplicativo que pergunte a idade de uma pessoa e diga se ela pode doar sangue ou não.
+                //Use alguns dos operadores lógicos OU(||) e E(&&).
+
+
+        }
+    }
+}
